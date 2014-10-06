487ca9b16cd02a0939ea26244e91215c0aec8b64
Micael Persson <micael.c.persson@gmail.com>
2014-10-06 21:13:30 +0200

    Started to work on the constructors.

--- 1DV402.S2.L02A/1DV402.S2.L02A/AlarmClock.cs
+++ 1DV402.S2.L02A/1DV402.S2.L02A/AlarmClock.cs
@@ -46,8 +46,7 @@ namespace _1DV402.S2.L02A
                 if (value < 0 || value > 23)
                 {
                     throw new ArgumentException();
-                } else {_hour = value;}
-                 
+                } else {_hour = value;}                 
             }
         }
         public int Minute
@@ -59,12 +58,20 @@ namespace _1DV402.S2.L02A
                 {
                     throw new ArgumentException();
                 }
-                else {_minute = value;}
-                 
+                else {_minute = value;}                 
             }
         }
-
-
+        AlarmClock(int hour, int minute)
+        {
+            
+        }
+        public AlarmClock(int hour, int minute , int alarmHour, int alarmMinute)
+        {
+            Hour = hour;
+            Minute = minute;
+            AlarmHour = alarmHour;
+            AlarmMinute = alarmMinute;
+        }
 
     }
 }
