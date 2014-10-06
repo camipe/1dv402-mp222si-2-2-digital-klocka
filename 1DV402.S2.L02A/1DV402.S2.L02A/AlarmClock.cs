c4de991ae59f79a37f01b18f3e980b76dfe2bd7d
Micael Persson <micael.c.persson@gmail.com>
2014-10-06 19:42:04 +0200

    Created class AlarmClock and the needed fields.

---
+++ 1DV402.S2.L02A/1DV402.S2.L02A/AlarmClock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L02A
+{
+    class AlarmClock
+    {
+        private int _alarmHour;
+        private int _alarmMinute;
+        private int _hour;
+        private int _minute;
+    }
+}
