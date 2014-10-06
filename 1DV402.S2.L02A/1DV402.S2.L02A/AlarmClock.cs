a3c086e5bd7ad66f53c2217f6fa9e4125a38c407
Micael Persson <micael.c.persson@gmail.com>
2014-10-06 20:10:18 +0200

    Encapsulated the fields in AlarmClock.cs

--- 1DV402.S2.L02A/1DV402.S2.L02A/AlarmClock.cs
+++ 1DV402.S2.L02A/1DV402.S2.L02A/AlarmClock.cs
@@ -8,9 +8,63 @@ namespace _1DV402.S2.L02A
 {
     class AlarmClock
     {
+        // Declaring fields
         private int _alarmHour;
         private int _alarmMinute;
         private int _hour;
         private int _minute;
+
+        // Encapsulating fields
+        public int AlarmHour
+        {
+            get { return _alarmHour; }
+            set 
+            {
+                if (value < 0 || value > 23)
+                {
+                    throw new ArgumentException();
+                } 
+                else{_alarmHour = value;}                
+            }
+        }
+        public int AlarmMinute
+        {
+            get { return _alarmMinute; }
+            set 
+            { 
+                if (value < 0 || value > 59)
+                {
+                    throw new ArgumentException();
+                } else {_alarmMinute = value;}
+            }
+        }
+        public int Hour
+        {
+            get { return _hour; }
+            set 
+            {
+                if (value < 0 || value > 23)
+                {
+                    throw new ArgumentException();
+                } else {_hour = value;}
+                 
+            }
+        }
+        public int Minute
+        {
+            get { return _minute; }
+            set 
+            {
+                if (value < 0 || value > 59)
+                {
+                    throw new ArgumentException();
+                }
+                else {_minute = value;}
+                 
+            }
+        }
+
+
+
     }
 }
