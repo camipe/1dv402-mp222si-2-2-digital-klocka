02c937da2b05f6073a4e53a4def8113cc9f70ab2
Micael Persson <micael.c.persson@gmail.com>
2014-10-08 15:50:52 +0200

    Finished test 6 and 7. Also added some comments.

--- 1DV402.S2.L02A/1DV402.S2.L02A/Program.cs
+++ 1DV402.S2.L02A/1DV402.S2.L02A/Program.cs
@@ -7,9 +7,11 @@ using System.Threading.Tasks;
 namespace _1DV402.S2.L02A
 {
     class Program
-    {
+    {        
         static void Main(string[] args)
-        {                      
+        {
+            //Running a series of test to check the class AlarmClock.
+            
             // Test 1
             AlarmClock clock = new AlarmClock();
             ViewTestHeader("Test 1.\nTest av standardkonstruktorn.");
@@ -48,9 +50,70 @@ namespace _1DV402.S2.L02A
             Console.WriteLine();
 
             // Test 6
+            ViewTestHeader("Test 6.\nTestar egenskaperna så att undantag kastas då tid och alarmtid tilldelas felaktiga värden.");
+            // Testing clock.Hour
+            try
+            {
+                clock.Hour = 24;
+            }
+            catch (ArgumentException)
+            {
+                ViewErrorMessage("Timmen är inte i intervallet 0-23.");                
+            }
+            // Testing clock.Minute
+            try
+            {
+                clock.Minute = 60;
+            }
+            catch (ArgumentException)
+            {
+                ViewErrorMessage("Minuten är inte i intervallet 0-59.");
+            }
+            // Testing clock.AlarmHour
+            try
+            {
+                clock.AlarmHour = 24;
+            }
+            catch (ArgumentException)
+            {
+                ViewErrorMessage("Alarmtimmen är inte i intervallet 0-23.");
+            }
+            // Testing clock.AlarmMinute
+            try
+            {
+                clock.AlarmHour = 60;
+            }
+            catch (ArgumentException)
+            {
+                ViewErrorMessage("Alarmtimmen är inte i intervallet 0-59.");
+            }
+            Console.WriteLine();
+
+            // Test 7
+            ViewTestHeader("Test 7.\nTestar konstruktorerna så att undantag kastas då tid och alarmtid tilldelas felaktiga värden.");
+            //Testing Hour/Minute constructor
+            try
+            {
+                clock = new AlarmClock(49, 39);
+            }
+            catch (ArgumentException)
+            {
+                ViewErrorMessage("Timmen/Minuten är inte i intervallet 0-23/0-59");
+            }
+            // Testing AlarmHour/AlarmMinute constructor
+            try
+            {
+                clock = new AlarmClock(35, 70);
+            }
+            catch (ArgumentException)
+            {
+                ViewErrorMessage("Alarmtimmen/Alarmminuten är inte i intervallet 0-23/0-59");
+            }
+            Console.WriteLine();
 
         }
 
+        // Method that runs the AlarClock.Ticktock method and if it returns true prints the special alarm message else it justs prints the time with no formatting.
         private static void Run(AlarmClock ac, int minutes)
         {
             for (int i = 0; i < minutes; i++)
@@ -66,9 +129,13 @@ namespace _1DV402.S2.L02A
             }
         }
 
+        // Methods that prints messages (in different styles).
         private static void ViewErrorMessage(string message)
         {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(message);
+            Console.ResetColor();
         }
 
         private static void ViewTestHeader(string header)
